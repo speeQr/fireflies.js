f8c188be783371ab97b9820045b4d38775f4d11c
Jeremy Gray <jeremy.gray@akqa.com>
2016-07-13 18:50:16 -0700

    Add files via upload

--- fireflies.js
+++ fireflies.js
@@ -3,6 +3,7 @@
  */
 
 (function() {
+    //instantiate our canvas vars
     var mainCanvas = document.getElementById("fireflyCanvas");
     var mainContext = mainCanvas.getContext('2d');
     var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
@@ -10,25 +11,44 @@
     var canvasHeight = mainCanvas.height;
     var midX = canvasWidth / 2;
     var midY = canvasHeight / 2;
-    var fireflies = [];
 
     var firefly = new Image();
-    firefly.src = '/assets/img/firefly.png';
+    firefly.src = './assets/img/firefly.png';
 
     firefly.onload = function() {
         createFireflies();
     };
 
+    //once image is loaded, we create the flies
+    var createFireflies = function() {
+        //create flies based on numFireflies
+        for (var i = 0; i < NUM_FIREFLIES; i++) {
+            new Firefly();
+        }
+    };
+
+    //function to draw every fly at their new positions on every frame.
+    var draw = function() {
+        mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        //updates every item in object
+        for (i in fireflies) {
+            fireflies[i].update();
+        }
+        requestAnimationFrame(draw);
+    };
+
     var fireflies = {},
-        fireflyIndex = 0,
-        numFireflies = 28;
+        fireflyIndex = 0;
+
+    const NUM_FIREFLIES = 128;
+    const FIREFLY_RADIUS = 250;
 
     var Firefly = function() {
-
         var settings = {
             alpha: 0,
             maxAlpha: Math.random() * 1 + .25,
-            radius: Math.random() * 250,
+            radius: Math.random() * FIREFLY_RADIUS,
             speed: .1 + Math.random() * 1.2,
             size: Math.random() * 22 + 12,
             xPos: Math.round(Math.random() * canvasWidth),
@@ -45,31 +65,49 @@
             }
         }
 
-        //set the direction of the circle motion
-        if (this.direction === 0) {
-            this.direction = -1;
-        }
-
         // Add new particle to the index
         fireflyIndex++;
         fireflies[fireflyIndex] = this;
         this.id = fireflyIndex;
 
-        //fade in
-        TweenLite.to(this, 1, { alpha: this.maxAlpha });
+        //set params and lifespan
+        this.setDirection();
+        this.setLifespan();
+    };
 
-        //if fly dies, fade to zero and generate a new one. 
-        var that = this;
+    Firefly.prototype.setDirection = function() {
+      //set the direction of the circle motion
+      if (this.direction === 0) {
+          this.direction = -1;
+      }
+    };
+
+    Firefly.prototype.setLifespan = function() {
+      //fade in
+      TweenLite.to(this, 1, { alpha: this.maxAlpha });
+
+      //if fly dies, fade to zero and generate a new one.
+      var that = this;
+      setTimeout(function() {
+          //fade out
+          TweenLite.to(that, 1, {
+              alpha: 0,
+              onComplete: that.populateFly,
+              onCompleteParams: [that.id],
+              overwrite: "auto"
+          });
+      }, this.maxLife);
+    };
+
+    Firefly.prototype.populateFly = function(id) {
+        delete fireflies[id];
+
+        var randNum = Math.random() * 10000;
         setTimeout(function() {
-            //fade out
-            TweenLite.to(that, 1, {
-                alpha: 0,
-                onComplete: populateFly,
-                onCompleteParams: [that.id],
-                overwrite: "auto"
-            });
-        }, this.maxLife);
-    }
+            new Firefly();
+        }, this.randNum);
+
+    };
 
     Firefly.prototype.update = function() {
         //movement
@@ -80,38 +118,9 @@
 
         mainContext.globalAlpha = this.alpha;
         mainContext.drawImage(firefly, newX, newY, this.size, this.size);
-    }
+    };
 
-    function populateFly(id) {
-        delete fireflies[id];
-
-        var that = this;
-        var randNum = Math.random() * 10000;
-
-        setTimeout(function() {
-            new Firefly();
-        }, that.randNum);
-
-    }
-
-    var createFireflies = function() {
-
-        //create flies based on numFireflies
-        for (var i = 0; i < numFireflies; i++) {
-            new Firefly();
-        }
-
-        draw();
-    }
-
-    var draw = function() {
-        mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
-
-        //updates every item in object
-        for (i in fireflies) {
-            fireflies[i].update();
-        }
-        requestAnimationFrame(draw);
-    }
+    //start drawing the flies
+    draw();
 
 })();
